f459eceed7583b5588f3eb2e755d251fa73b7a16
Amit Sinha <aamitsinha456@gmail.com>
2022-08-30 13:38:09 +0530

    Updates

---
+++ precache-manifest.53bb0c0a0bbc163c5ab24b063e5da24d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d246dc13a6ff2f0de3ac6cac4cc161d9",
+    "url": "/chatApp/index.html"
+  },
+  {
+    "revision": "6a3a7995774abcf300bb",
+    "url": "/chatApp/static/css/main.0bb44d80.chunk.css"
+  },
+  {
+    "revision": "88fe6902d3c940f207af",
+    "url": "/chatApp/static/js/2.8fa09e46.chunk.js"
+  },
+  {
+    "revision": "6a3a7995774abcf300bb",
+    "url": "/chatApp/static/js/main.137f1974.chunk.js"
+  },
+  {
+    "revision": "809e9b1df6c56c4d2060",
+    "url": "/chatApp/static/js/runtime-main.c35e4976.js"
+  },
+  {
+    "revision": "44d3b27b4f4ad78d3b6eeccae8c01ef4",
+    "url": "/chatApp/static/media/search.44d3b27b.svg"
+  }
+]);
